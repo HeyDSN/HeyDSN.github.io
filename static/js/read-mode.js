67ff28e6d1bb31da60f63fb7060116ab25317750
Deni Setiawan <zerosite@users.noreply.github.com>
2017-05-10 09:37:54 +0700

    Disable read mode on small screen display

--- static/js/read-mode.js
+++ static/js/read-mode.js
@@ -7,31 +7,18 @@ $(".read-button").click(function() {
     $this.toggleClass("show");
     $divBtnGrp.toggleClass("x-margin");
     $divContainer.toggleClass("read-mode");
-    $(".banneradtop").toggle();
+    $divSide.toggleClass("hide-el");
+    $divDisqus.toggleClass("hide-el");
+    $(".banneradtop").toggleClass("hide-el");
     if ($this.hasClass("show")) {
-        $divSide.hide('slow');
-        $divDisqus.hide();
         document.body.style.backgroundColor = '#ddd';
-        $('h1').css({ 'color': '#000' });
-        $('h2').css({ 'color': '#000' });
-        $('h3').css({'color' : '#000'});
+        $('h1').css({ 'color' : '#000' });
+        $('h2').css({ 'color' : '#000' });
+        $('h3').css({ 'color' : '#000' });
     } else {
-        $divSide.show('slow');
-        $divDisqus.show();
         document.body.style.backgroundColor = '#fff';
-        $('h1').css({ 'color': '#696969' });
-        $('h2').css({ 'color': '#696969' });
-        $('h3').css({'color' : '#696969'});
+        $('h1').css({ 'color' : '#696969' });
+        $('h2').css({ 'color' : '#696969' });
+        $('h3').css({ 'color' : '#696969' });
     }
 });
-
-/*
-var div = $('someDiv');
-console.log(div.style('color'));
-div.style('color', 'red');
-console.log(div.style('color'));
-div.style('color', 'blue', 'important');
-console.log(div.style('color'));
-console.log(div.style().getPropertyPriority('color'));
-*/
-//document.getElementById("sidebar").style.display = "none";
